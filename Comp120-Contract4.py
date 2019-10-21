71977907a07cbc67fc7eaf898b66fcbbe483aa46
David Brown <gamesdevdave@gmail.com>
2019-10-21 21:19:09 +0100

    Comp120-Contract4.py updated: - Image manipulation functions added:   - Placing image onto surface.   - Changes the colour of the image.

--- Comp120-Contract4.py
+++ Comp120-Contract4.py
@@ -2,20 +2,33 @@ import sys
 
 import pygame
 
-WHITE = (255, 255, 255)
-
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("Contract-4-Program")
+screen.fill((255, 255, 255))
 
-xPosBox = 200
-yPosBox = 200
 
-pygame.draw.rect(screen, WHITE, (xPosBox, yPosBox, 20, 20))
-pygame.display.update()
+def addImage():
+    entity = pygame.image.load("Ground.png").convert_alpha()
+    screen.blit(entity, (0, 0))
+    pygame.display.flip()
+
+
+def entityColourChanger(surface=pygame.Surface((1, 1))):
+    pixel = pygame.Color(0, 0, 0)
+    for x in range(surface.get_width()):
+        for y in range(surface.get_height()):
+            pixel = surface.get_at((x, y))
+            surface.set_at(
+                (x, y),
+                pygame.Color(int(pixel.r * 0.5), pixel.g, pixel.b)
+            )
+
 
 while 1:
-    pygame.display.update()
+    addImage()
+    pygame.display.flip()
+    entityColourChanger(screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
