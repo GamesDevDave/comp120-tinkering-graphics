b5030fdb2c0ad0a85ba2eaa248ae94beae9cd7df
David Brown <gamesdevdave@gmail.com>
2019-10-21 21:54:22 +0100

    Comp120-Contract4.py updated: - Changed addImage function to not include pygame.display.flip() as it was messing with the game loop. - Added minor comments.

--- Comp120-Contract4.py
+++ Comp120-Contract4.py
@@ -1,17 +1,21 @@
+# Comp120-Contract4
+# David Brown
+# Last Updated: 21/10/2019.
+
+# Pygame and sys are imported for further use in the program.
 import sys
 
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((400, 400))
+
+pygame.init()   # Pygame is initialised, all its functions are now usable.
+screen = pygame.display.set_mode((400, 400))    # Sets the size of the screen.
 pygame.display.set_caption("Contract-4-Program")
-screen.fill((255, 255, 255))
 
 
 def addImage():
-    entity = pygame.image.load("Ground.png").convert_alpha()
+    entity = pygame.image.load("Ground.png").convert()
     screen.blit(entity, (0, 0))
-    pygame.display.flip()
 
 
 def entityColourChanger(surface=pygame.Surface((1, 1))):
@@ -21,14 +25,18 @@ def entityColourChanger(surface=pygame.Surface((1, 1))):
             pixel = surface.get_at((x, y))
             surface.set_at(
                 (x, y),
-                pygame.Color(int(pixel.r * 0.5), pixel.g, pixel.b)
+                pygame.Color(int(pixel.r), int(pixel.g * 0.1), int(pixel.b))
             )
 
 
-while 1:
-    addImage()
-    pygame.display.flip()
-    entityColourChanger(screen)
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    screen.fill((255, 255, 255))
+    addImage()
+    entityColourChanger(screen)
+    pygame.display.flip()
+
+pygame.quit()
