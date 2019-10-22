31570056ad5bf90f5a343d6e6717e5d847951bd0
David Brown <gamesdevdave@gmail.com>
2019-10-22 01:27:10 +0100

    Comp120-Contract4.py updated. - Comments added to show the flow of the program.

--- Comp120-Contract4.py
+++ Comp120-Contract4.py
@@ -8,16 +8,21 @@ import sys
 import pygame
 from pygame.tests.test_utils import png
 
-pygame.init()   # Pygame is initialised, all its functions are now usable.
-screen = pygame.display.set_mode((400, 400))    # Sets the size of the screen.
+# Pygame is initialised, all its functions are now usable.
+pygame.init()
+# Sets the size of the screen.
+screen = pygame.display.set_mode((400, 400))
+# Sets the tag on the top bar of the window to say Contract-4-Program
 pygame.display.set_caption("Contract-4-Program")
 
 
+# This loads in the original image and ensures that it is casted onto the screen.
 def addImage():
     entity = pygame.image.load("Ground.png").convert()
     screen.blit(entity, (0, 0))
 
 
+# This function changes the colour of the image depending on the values on line 33.
 def entityColourChanger(surface=pygame.Surface((1, 1))):
     pixel = pygame.Color(0, 0, 0)
     for x in range(surface.get_width()):
@@ -28,11 +33,13 @@ def entityColourChanger(surface=pygame.Surface((1, 1))):
                 pygame.Color(int(pixel.r), int(pixel.g * 0.1), int(pixel.b))
             )
 
-
+# Main game loop, continues until the user presses the exit button.
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+# This calls the functions needed to change the colour of the asset and saves the picture.
 
     screen.fill((255, 255, 255))
     addImage()
